dd3aa00e2fc2ba07fc3a889b91fa3b736a81c04a
Zach Aschim <aschza01@luther.edu>
2013-10-24 18:27:34 -0500

    First version with modal and wizard implementation

---
+++ src/static/modal.js
@@ -0,0 +1,32 @@
+
+$(function() {
+	$("#rideForm").formwizard({ 
+		formPluginEnabled: true,
+	 	validationEnabled: true,
+	 	focusFirstInput : true,
+	 	formOptions :{
+			success: function(data){$("#status").fadeTo(500,1,function(){ $(this).html("You just created a ride!").fadeTo(5000, 0);}),location.reload()}, 
+			beforeSubmit: function(data){$("#data").html("data sent to the server: " + $.param(data));},
+			dataType: 'json',
+			resetForm: true
+			}	
+	 	});
+  	});
+
+
+/////////////////////////////////////////////////////
+
+
+$(function() {
+	$("#eventForm").formwizard({ 
+		formPluginEnabled: true,
+	 	validationEnabled: true,
+	 	focusFirstInput : true,
+	 	formOptions :{
+			success: function(data){$("#status").fadeTo(500,1,function(){ $(this).html("You just created a ride!").fadeTo(5000, 0);}),location.reload()}, 
+			beforeSubmit: function(data){$("#data").html("data sent to the server: " + $.param(data));},
+			dataType: 'json',
+			resetForm: true
+			}	
+	 	});
+  	});
