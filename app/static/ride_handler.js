5ac42508183aa325d8406b48de127544af2098b5
Gus <bananagus@gmail.com>
2014-06-11 14:02:12 -0500

    removed .pyc files

---
+++ app/static/ride_handler.js
@@ -0,0 +1,41 @@
+document.body.addEventListener('click', function (e) {
+    var target = e.target;
+
+    if (target.dataset.driver) {
+        send('driver', target.dataset.driver);
+    }
+    if (target.dataset.passenger) {
+        send('passenger', target.dataset.passenger);
+    }
+}.bind(this));
+
+var send = function (type, action) {
+	var data = {};
+	data.type = type;
+	data.action = action;
+	
+	var push = $.ajax({
+		type: 'POST',
+		url: window.location.pathname,
+		dataType: 'json',
+		contentType: 'application/json; charset=UTF-8',
+		data: JSON.stringify(data)
+	});
+
+	push.done(function (data) {
+		notify({
+			type: 'success',
+			strong: data.strong,
+			message: data.message
+		});
+	});
+
+	push.fail(function (data, status) {
+		notify({
+			type: 'danger',
+			strong: data.strong,
+			message: data.message
+		});
+	});
+}
+
