932c9584ce9ad2cd6dd6b07e2fd4b42fedc0dfce
Gus <bananagus@gmail.com>
2014-06-13 11:25:56 -0500

    removed more handlers from main.py

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ import datetime
 from datetime import date
 from google.appengine.api import mail
 
-
 import jinja2
 from google.appengine.ext import db
 
@@ -43,8 +42,6 @@ import urllib
 import random
 import os.path
 
-
-
 from app.controllers.account_flow import *
 from app.controllers.circles import *
 from app.controllers.events import *
@@ -53,7 +50,6 @@ from app.controllers.comments import *
 from app.controllers.users import *
 
 from app.common.toolbox import doRender
-
 
 ## Testing Code
 aquery = db.Query(Community)
@@ -70,7 +66,6 @@ if aquery.count() == 0:
  
 
 class MainHandler(BaseHandler):
-
   def get(self):
     self.auth()
     user = self.current_user()
@@ -95,24 +90,6 @@ class MapHandler(BaseHandler):
             'user': user
         })
 
-class SubmitRatingHandler(BaseHandler):
-    def post(self):
-        aquery = db.Query(College)
-        mycollege = aquery.get()  
-        drivernum = self.request.get("driver")
-        text = self.request.get("ratetext")
-        ooFrating = self.request.get("ooFrating") # Out Of Five
-        user = FBUser.get_by_key_name(drivernum)
-        user.drivercomments.append(text)
-        user.numrates = user.numrates + 1
-        if user.rating == None:
-            user.rating = float(ooFrating)
-        else:
-            user.rating = user.rating + float(ooFrating)
-        user.put()
-        doRender(self, "submit.html", {"college", mycollege})
-        self.redirect("/home")
-
 class HomeHandler(BaseHandler):
     def get(self):
         self.auth()
@@ -124,151 +101,12 @@ class HomeHandler(BaseHandler):
             'user': user
         })
 
-class RateHandler(BaseHandler):
-    
-    def get(self):
-      aquery = db.Query(College)
-      mycollege= aquery.get()  
-      drivernum = self.request.get('dkey')
-      user = FBUser.get_by_key_name(drivernum)
-      doRender(self, 'ratedriver.html', {
-            'driver': user.nickname(),
-            'drivernum':drivernum,
-            'college':mycollege
-            })
-      
-class RemovePassengerHandler(BaseHandler):
-    """
-    Removes a passenger using a key and the current user
-    """
-    def get(self):
-      rkey = self.request.get('rkey')
-      ride = db.get(rkey)
-      pkey = self.request.get('pkey')
-      passenger = db.get(pkey)
-      #self.response.out.write('Would remove %s from %s ride' % (passenger.name, ride.driver))
-      if ride == None:
-        doRender(self, 'error.html', {
-                              'error_message': "No such ride exists."})
-      elif passenger == None:
-        doRender(self, 'error.html', {
-                              'error_message': "No such passenger exists."})
-      else:
-        name = passenger.name
-        ride.passengers.remove(passenger.key())
-        passenger.delete()
-        ride.num_passengers -= 1
-        ride.put()
-        query = db.Query(Ride)
-        query.filter("ToD > ", datetime.date.today())
-        ride_list = query.fetch(limit=100)
-        user = self.current_user
-        aquery = db.Query(College)
-        mycollege= aquery.get()
-        greeting = ''
-        if user:
-            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>) Go to your <a href='/home'>Home Page</a>" %
-                  (user.nickname(), users.create_logout_url("/")))
-        message = '%s has been removed from %s\'s ride.' % (name, ride.driver)
-        doRender(self, 'map.html', {
-            'ride_list': ride_list, 
-            'greeting' : greeting,
-            'college': mycollege,
-            'address': mycollege.address,
-            'nick' : user.nickname(),
-            'logout':'/auth/logout',
-            'mapkey':MAP_APIKEY,
-        })
-
-class DriverRatingHandler(BaseHandler):
-
-    def get(self):
-        drivernum = self.request.get('drivernum')
-        user = FBUser.get_by_key_name(drivernum)
-        ratingslist= user.drivercomments
-        name = user.nickname()
-        if user.rating != None:
-            rating = user.rating / user.numrates
-        else:
-            rating= 0.00
-        numrates = user.numrates
-      
-        doRender(self, 'driverrating.html', {
-            'ratingslist': ratingslist,
-            'name': name,
-            'rating':str(rating)[0:3],
-            'numrates':numrates
-        })
-
-class SchoolErrorHandler(BaseHandler):
-    
-    def get(self):
-      aquery = db.Query(College)
-      mycollege= aquery.get() 
-      doRender(self, 'schoolerror.html', {"college": mycollege})
-
-class RideSuccessHandler(BaseHandler):
-    def get(self):
-       aquery = db.Query(College)
-       mycollege= aquery.get() 
-       noDriver = 0
-       noPass = 0
-       goodRide = 0
-       myquery = db.Query(Ride)
-       rides = myquery.fetch(limit=1000000)
-       for ride in rides:
-           logging.debug(ride.driver)
-           if ride.driver == None:
-               noDriver += 1
-           else:
-               if ride.num_passengers >0:
-                   goodRide += 1
-               else:
-                   noPass += 1
-       doRender(self, 'ridesuccess.html', {
-            'noDriver': noDriver,
-            'noPass': noPass,
-            'goodRide': goodRide,
-            'totalRides': len(rides),
-            'college': mycollege
-        })   
-
 class ConnectPageHandler(BaseHandler):
-
     def get(self):
         user = self.current_user
         userID = user.id
         doRender(self, 'connectride.html',{
             'drivernum': userID
-        })
-
-class DatabaseHandler(BaseHandler):
-    def get(self):
-        ID = self.request.get('ID', None)
-        
-        if ID is None:
-            # First request, just get the first name out of the datastore.
-            ride = db.Query(Ride).get()
-            ID = ride.ID
-
-        q = db.Query(Ride)
-        rides = q.fetch(limit=2)
-        current_ride = rides[0]
-        if len(rides) == 2:
-            next_ride = rides[1].ID
-            next_url = '/update?ID=%s' % urllib.quote(next_ride)
-        else:
-            next_ride = 'FINISHED'
-            next_url = '/'  # Finished processing, go back to main page.
-        # In this example, the default values of 0 for num_votes and avg_rating are
-        # acceptable, so we don't need to do anything other than call put().
-        if not isinstance(current_ride.driver,str):
-           current_ride.put()
-
-        doRender(self, 'update.html', {
-            'current_name': ID,
-            'next_name': next_ride,
-            'next_url': next_url,
         })
 
 class IncorrectHandler(BaseHandler):
